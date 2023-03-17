3b791fb1d9c214a71fb5a231601d9e085aad3064
JEMartinezz <123746302+JEMartinezz@users.noreply.github.com>
2023-03-17 10:49:02 -0500

    Add files via upload
    Avance...

---
+++ Turnos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Turnos
+{
+	public static void main(String[] args) {
+		
+		int NPersonas; 
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("ingresar el numero de personas a las que se le asignan los turnos: ")
+		
+		for(int=0;i <NPersonas;i++)
+	} 
+}
